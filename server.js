69e45a7853f552035cc3032bc3d39a57362bf015
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-04-21 13:14:19 -0700

    added delete

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 require('dotenv').config();
 // Application dependencies
 const express = require('express');
@@ -21,16 +21,18 @@ app.use(express.urlencoded({extended:true}));
 // Application Middleware
 app.use(cors());
 
+app.get('/test', (req, res) => res.send('hello world'));
+
 // API Endpoints
 app.get('/api/v1/books/:id', (req, res) => {
   client.query(`SELECT book_id, title, author, image_url, isbn FROM books 
   WHERE title = $1;`)
-  [request.body.title]
+    [request.body.title]
     .then(results => res.send(results.rows))
     .catch(console.error);
 });
 app.post('/api/v1/books/:id', (req, res) => {
-  client.query(`INSERT INTO things (name) VALUES('${req.body.name}');
+  client.query(`INSERT INTO books VALUES('${req.body.name}');
   `).then(results => res.send(results.rows))
     .catch(err => res.sendStatus(500));
 
@@ -40,7 +42,7 @@ app.post('/api/v1/books/:id', (req, res) => {
 
 app.put('/api/v1/books/:id', (req, res) => {
   //apiURL + id in parenthesis
-  client.query(`UPDATE tablename SET name='${req.body.name}' 
+  client.query(`UPDATE books SET name='${req.body.name}' 
   WHERE id=${req.params.id};
   
   `).then(results => res.send(results.rows))
@@ -50,7 +52,12 @@ app.put('/api/v1/books/:id', (req, res) => {
 
 });
 
-//DO THE DELETE HERE FROM LECTURE
 
+app.delete('/api/v1/books/:id', (req, res) => {
+  client.query(`
+  DELETE FROM books WHERE id=${req.params.id};`)
+    .then(results => res.send(results.rows))
+    .catch(err => res.sendStatus(500));
+});
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
